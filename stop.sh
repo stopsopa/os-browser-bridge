7f5128b0a7a9c729a6d05943f6c7b1b885dc3a20
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-03-13 19:38:22 +0000

    d

--- stop.sh
+++ stop.sh
@@ -1,3 +1,10 @@
 
-pkill -f "server/index.js --flag=osbridgeserver"
+
+# set -e
+set -x
+ps aux | grep -v grep | grep osbridgeserver
+ps aux | grep -v grep | grep joboffers
+# and now pause wait for user input
+read -p "Press [Enter] key to continue..."
+ps aux | grep -v grep | grep joboffers | /bin/bash bash/proc/reaper.sh
 exit 0
