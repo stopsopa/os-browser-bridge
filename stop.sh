d4c0bb7cd9ceee5133c66f90fdadbeeeb83476ed
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-04-16 14:50:53 +0100

    d

--- stop.sh
+++ stop.sh
@@ -3,8 +3,7 @@
 # set -e
 set -x
 ps aux | grep -v grep | grep osbridgeserver
-ps aux | grep -v grep | grep joboffers
 # and now pause wait for user input
 read -p "Press [Enter] key to continue..."
-ps aux | grep -v grep | grep joboffers | /bin/bash bash/proc/reaper.sh
+ps aux | grep -v grep | grep osbridgeserver | /bin/bash bash/proc/reaper.sh
 exit 0
